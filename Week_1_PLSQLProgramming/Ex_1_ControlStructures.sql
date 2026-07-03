9eec152562e2bb92f0dff288b334de95c2753a80
Harshitha-965 <harshithakg09@gmail.com>
2026-07-03 20:34:59 +0530

    Update Ex_1_ControlStructures.sql

--- Week_1_PLSQLProgramming/Ex_1_ControlStructures.sql
+++ Week_1_PLSQLProgramming/Ex_1_ControlStructures.sql
@@ -7,9 +7,36 @@
 -- Apply a 1% discount to loan interest rates for
 -- customers above 60 years old.
 -- ------------------------------------------------------
+SET SERVEROUTPUT ON;
 
+DECLARE
+    v_age NUMBER;
+    CURSOR customer_cursor IS
+        SELECT CUSTOMERID,DOB
+        FROM CUSTOMERS;
+BEGIN
+    FOR customer_record IN customer_cursor LOOP
+        v_age := FLOOR(MONTHS_BETWEEN(SYSDATE,customer_record.DOB) / 12);
 
+        DBMS_OUTPUT.PUT_LINE(
+            'Customer ID: ' || customer_record.CUSTOMERID ||
+            ' Age: ' || v_age
+        );
 
+        if v_age > 60 THEN
+
+            UPDATE LOANS
+            SET INTERESTRATE = INTERESTRATE-1
+            WHERE CUSTOMERID = customer_record.CUSTOMERID;
+
+            DBMS_OUTPUT.PUT_LINE(
+                'Interest rate updated.'
+            );
+
+        END IF;
+    END LOOP;
+END;
+/
 -- ------------------------------------------------------
 -- Scenario 2
 -- Promote customers with balance greater than $10,000

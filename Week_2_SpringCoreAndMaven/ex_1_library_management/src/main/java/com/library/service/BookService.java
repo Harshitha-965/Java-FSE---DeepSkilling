7bc6cc60870381e6e8081fc32cffcff525094212
Harshitha-965 <harshithakg09@gmail.com>
2026-07-07 13:18:29 +0530

    Completed Ex-2 Spring Core and Maven- Dependency Injection.

--- Week_2_SpringCoreAndMaven/ex_1_library_management/src/main/java/com/library/service/BookService.java
+++ Week_2_SpringCoreAndMaven/ex_1_library_management/src/main/java/com/library/service/BookService.java
@@ -1,7 +1,5 @@
 package com.library.service;
 
-import com.library.repository.BookRepository;
-
 public class BookService {
     public BookService(){
         System.out.println("BookService bean created");

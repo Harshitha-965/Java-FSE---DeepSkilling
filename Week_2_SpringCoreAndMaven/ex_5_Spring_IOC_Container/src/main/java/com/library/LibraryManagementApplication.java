9393b1635d29b2f466b7aaba89c04d60613d1dff
Harshitha-965 <harshithakg09@gmail.com>
2026-07-08 09:38:39 +0530

    Completed Ex-5 Spring Core and Maven - Configuring Spring IoC Container

---
+++ Week_2_SpringCoreAndMaven/ex_5_Spring_IOC_Container/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,22 @@
+package com.library;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+
+public class LibraryManagementApplication {
+    
+    public static void main(String args[]){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookRepository repository = context.getBean("bookRepository",BookRepository.class);
+
+        BookService service = context.getBean("bookService",BookService.class);
+
+        repository.displayRepository();
+        service.displayService();
+
+        context.close();
+    }
+}

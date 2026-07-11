79081ed1141aeff3c6b424d0f7196569e4395a37
Harshitha-965 <harshithakg09@gmail.com>
2026-07-11 13:36:00 +0530

    Completed Ex-4 Spring REST using SpringBoot3

---
+++ Week_3_SpringREST_SpringBoot3/Country_With_Country_Code/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,25 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    private final CountryService countryService;
+
+    CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
+
+    }
+}

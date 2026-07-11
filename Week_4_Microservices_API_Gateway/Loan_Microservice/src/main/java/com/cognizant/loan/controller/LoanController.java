a303f13217b65f0ab02c2e03c63046dd8bc84365
Harshitha-965 <harshithakg09@gmail.com>
2026-07-11 20:32:31 +0530

    Completed Week-4 Microservices using API Gateway

---
+++ Week_4_Microservices_API_Gateway/Loan_Microservice/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,23 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.loan.model.Loan;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+
+        return new Loan(
+                number,
+                "Home Loan",
+                5000000.00,
+                32500,
+                25
+        );
+    }
+}
